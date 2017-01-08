c3be080cd48ac4a71c758aa139b5ca69eb17197d
IlanZerdoun <ilanzerdoun@macbook-pro-de-ilan.home>
2017-01-08 01:45:25 +0100

    Update 1.2
    Evolution JS
    Box, Slide …

--- JS/Nike.js
+++ JS/Nike.js
@@ -1,6 +1,70 @@
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 3,
+var swiper = new Swiper('.swiper-container', {
+        pagination: 'none',
+        slidesPerView: 4,
         paginationClickable: true,
-        spaceBetween: 30
-    });
+        spaceBetween: 30,
+        loop: true,
+});
+
+$('#heart').click(function(){
+    if($(this).hasClass('social-network')){
+        $(this).removeClass('social-network');
+        $(this).attr('src', 'IMG/Icones/heart1.png');
+    } else {
+        $(this).addClass('social-network');
+        $(this).attr('src', 'IMG/Icones/heart.png');
+    }
+});
+
+
+$(document).ready(function(){
+        $('#img').click(function(){
+                $('#big-shoes').attr('src','IMG/Shoes-choose1.png');
+                $('#shoes-change').attr('src','IMG/Shoes-choose1.png');
+            });
+});
+
+$(document).ready(function(){
+        $('#img1').click(function(){
+                $('#big-shoes').attr('src','IMG/Shoes-choose.png');
+                $('#shoes-change').attr('src','IMG/Shoes-choose.png');
+            });
+});
+$(document).ready(function(){
+		$("#search").click(function(){
+			$("#search").html("<input type='text' placeholder='Search'/>").css("transition", "0.9s");
+		});
+});
+
+jQuery(function($) {
+  function fixDiv() {
+      
+    var $cache = $('#box');
+      
+    if ($(window).scrollTop() < 560){
+        
+        $cache.fadeOut().css({
+        'position': 'absolute',    
+        });
+
+    }
+    
+    else if ($(window).scrollTop() > 560 && $(window).scrollTop() < 1359){
+        $cache.fadeIn().css({
+        'position': 'fixed',
+        'right': '93px', 
+        });
+
+    }
+    else{
+        
+        $cache.css({
+        'position': 'absolute',
+        'right': '48px',
+        });
+  }}
+    
+  $(window).scroll(fixDiv);
+  fixDiv();
+});
+
